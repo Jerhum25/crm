06fca676c411434b379f0ae841e138a513a45ae0
jerhum <jerhum@msn.com>
2025-06-22 18:59:04 +0200

    modif css stats

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -32,29 +32,29 @@ function Statistics() {
     return Math.floor(diffTime / (1000 * 60 * 60 * 24));
   };
 
-const exportToCSV = (data, filename) => {
-  if (!data || data.length === 0) return;
+  const exportToCSV = (data, filename) => {
+    if (!data || data.length === 0) return;
 
-  const separator = ";";
-  const header = Object.keys(data[0]).join(separator);
-  const rows = data
-    .map((obj) =>
-      Object.values(obj)
-        .map((val) => (val !== null && val !== undefined ? `"${val}"` : ""))
-        .join(separator)
-    )
-    .join("\n");
+    const separator = ";";
+    const header = Object.keys(data[0]).join(separator);
+    const rows = data
+      .map((obj) =>
+        Object.values(obj)
+          .map((val) => (val !== null && val !== undefined ? `"${val}"` : ""))
+          .join(separator)
+      )
+      .join("\n");
 
-  const csv = "\uFEFF" + header + "\n" + rows; // ajout du BOM
+    const csv = "\uFEFF" + header + "\n" + rows; // ajout du BOM
 
-  const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
-  const link = document.createElement("a");
-  link.href = URL.createObjectURL(blob);
-  link.setAttribute("download", `${filename}.csv`);
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-};
+    const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.setAttribute("download", `${filename}.csv`);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   const relaunchClients = clientsData.filter((client) => {
     const days = getDateDiffInDays(client.lastContact);
@@ -91,32 +91,34 @@ const exportToCSV = (data, filename) => {
 
       <div className="clientsStats">
         <h3>Répartition par statut </h3>
-        <p>
+        {/* <p>
           <span>Total : </span>
           {clientsData.length}
-        </p>
-        <p>
-          <span style={{ backgroundColor: "#f8e263" }}>Prospects</span>
-          {
-            clientsData.filter((client) => client.clientStatus === "prospect")
-              .length
-          }
-        </p>
-        <p>
-          <span style={{ backgroundColor: "#7bffa7" }}>Clients</span>
-          {
-            clientsData.filter((client) => client.clientStatus === "client")
-              .length
-          }
-        </p>
-        <p>
-          <span style={{ backgroundColor: "#eb6565" }}>Anciens clients</span>
-          {
-            clientsData.filter(
-              (client) => client.clientStatus === "ancien client"
-            ).length
-          }
-        </p>
+        </p> */}
+        <div className="clientsStatus">
+          <p>
+            <span style={{ backgroundColor: "#f8e263" }}>Prospects</span>
+            {
+              clientsData.filter((client) => client.clientStatus === "prospect")
+                .length
+            }
+          </p>
+          <p>
+            <span style={{ backgroundColor: "#7bffa7" }}>Clients</span>
+            {
+              clientsData.filter((client) => client.clientStatus === "client")
+                .length
+            }
+          </p>
+          <p>
+            <span style={{ backgroundColor: "#eb6565" }}>Anciens clients</span>
+            {
+              clientsData.filter(
+                (client) => client.clientStatus === "ancien client"
+              ).length
+            }
+          </p>
+        </div>
         <div className="progressBar">
           <div
             className="progressBarProspects"
@@ -171,7 +173,7 @@ const exportToCSV = (data, filename) => {
                 {new Date(
                   client.lastContact?.toDate?.() || client.lastContact
                 ).toLocaleDateString("fr-FR")}{" "}
-                —{client.notes}
+                — {client.notes}
               </li>
             ))}
         </ul>
@@ -184,13 +186,17 @@ const exportToCSV = (data, filename) => {
       </div>
 
       <div className="relaunchSection">
-        <h3>Clients à relancer (+30 jours)</h3>
+        <h3>
+          {relaunchClients.length <= 1
+            ? "Client à relancer (+30 jours)"
+            : "Clients à relancer (+30 jours)"}
+        </h3>
         <ul>
           {relaunchClients.map((c) => (
             <li key={c.id}>
               <p>
                 <strong>
-                  {c.firstName} <span>{c.lastName}</span>
+                  <span>{c.firstName}</span> {c.lastName}
                 </strong>{" "}
                 – Dernier contact :{" "}
                 {new Date(c.lastContact).toLocaleDateString("fr-FR")}
@@ -207,7 +213,11 @@ const exportToCSV = (data, filename) => {
       </div>
 
       <div className="noLastContact">
-        <h3>Client(s) non contacté(s)</h3>
+        <h3>
+          {clientsData.filter((client) => !client.lastContact).length <= 1
+            ? "Client non contacté"
+            : "Clients non contactés"}
+        </h3>
         {clientsData.filter((client) => !client.lastContact).length === 0 ? (
           <p>Tous les clients ont une date contact.</p>
         ) : (
@@ -226,13 +236,14 @@ const exportToCSV = (data, filename) => {
                   </li>
                 ))}
             </ul>
-                    <button
-          className="exportCSV"
-          onClick={() => exportToCSV(noContactClients, "clients-sans-contact")}
-        >
-          Exporter en CSV
-        </button>
-
+            <button
+              className="exportCSV"
+              onClick={() =>
+                exportToCSV(noContactClients, "clients-sans-contact")
+              }
+            >
+              Exporter en CSV
+            </button>
           </>
         )}
       </div>
